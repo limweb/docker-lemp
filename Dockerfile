1ed63b7cd4055f0d1f577e606027c8b69b458ad0
lim web <limweb@hotmail.com>
2020-01-07 11:22:20 +0700

    updatedockerfile

--- Dockerfile
+++ Dockerfile
@@ -11,20 +11,20 @@ RUN apk add -U nano
 COPY php/php-fpm.ini /etc/supervisor.d/
 
 # mysql
-# RUN apk add mysql mysql-client
-# COPY mysql/mysqld.ini /etc/supervisor.d/
+RUN apk add mysql mysql-client
+COPY mysql/mysqld.ini /etc/supervisor.d/
 
 # pgsql
-# RUN apk add postgresql
-# COPY pgsql/postgres.ini /etc/supervisor.d/
+RUN apk add postgresql
+COPY pgsql/postgres.ini /etc/supervisor.d/
 
 # redis
-# RUN apk add redis
-# COPY redis/redis-server.ini /etc/supervisor.d/
+RUN apk add redis
+COPY redis/redis-server.ini /etc/supervisor.d/
 
 # beankstalkd
-# RUN apk add beanstalkd
-# COPY beanstalkd/beanstalkd.ini /etc/supervisor.d/
+RUN apk add beanstalkd
+COPY beanstalkd/beanstalkd.ini /etc/supervisor.d/
 
 # nginx
 RUN \
@@ -37,18 +37,18 @@ COPY nginx/conf.d/default.conf /etc/nginx/conf.d/default.conf
 COPY nginx/conf/nginx-site.conf /etc/nginx/sites-available/default.conf
 
 # mailcatcher
-# COPY --from=tophfr/mailcatcher /usr/lib/libruby.so.2.5 /usr/lib/libruby.so.2.5
-# COPY --from=tophfr/mailcatcher /usr/lib/ruby/ /usr/lib/ruby/
-# COPY --from=tophfr/mailcatcher /usr/bin/ruby /usr/bin/mailcatcher /usr/bin/
-# COPY mail/mailcatcher.ini /etc/supervisor.d/
+COPY --from=tophfr/mailcatcher /usr/lib/libruby.so.2.5 /usr/lib/libruby.so.2.5
+COPY --from=tophfr/mailcatcher /usr/lib/ruby/ /usr/lib/ruby/
+COPY --from=tophfr/mailcatcher /usr/bin/ruby /usr/bin/mailcatcher /usr/bin/
+COPY mail/mailcatcher.ini /etc/supervisor.d/
 
 # supervisor
 RUN apk add supervisor
 
 # adminer
-# RUN \
-#   mkdir -p /var/www/adminer \
-#   && curl -sSLo /var/www/adminer/index.php "https://github.com/vrana/adminer/releases/download/v$ADMINER_VERSION/adminer-$ADMINER_VERSION-en.php"
+RUN \
+  mkdir -p /var/www/adminer \
+  && curl -sSLo /var/www/adminer/index.php "https://github.com/vrana/adminer/releases/download/v$ADMINER_VERSION/adminer-$ADMINER_VERSION-en.php"
 
 
 
@@ -58,7 +58,7 @@ ADD scripts/pull /usr/bin/pull
 ADD scripts/push /usr/bin/push
 ADD scripts/letsencrypt-setup /usr/bin/letsencrypt-setup
 ADD scripts/letsencrypt-renew /usr/bin/letsencrypt-renew
-# RUN chmod 755 /usr/bin/pull && chmod 755 /usr/bin/push && chmod 755 /usr/bin/letsencrypt-setup && chmod 755 /usr/bin/letsencrypt-renew && chmod 755 /start.sh
+RUN chmod 755 /usr/bin/pull && chmod 755 /usr/bin/push && chmod 755 /usr/bin/letsencrypt-setup && chmod 755 /usr/bin/letsencrypt-renew && chmod 755 /start.sh
 
 # resource
 COPY php/index.php /var/www/html/index.php
@@ -81,8 +81,7 @@ RUN chmod +x /docker-entrypoint.sh
 RUN \
   rm -rf /var/cache/apk/* /tmp/* /var/tmp/* /usr/share/doc/* /usr/share/man/*
 
-# EXPOSE 11300 9000 6379 5432 3306 88 80
-EXPOSE 80 443
+EXPOSE 11300 9000 6379 5432 3306 88 80
 
 ENTRYPOINT ["/docker-entrypoint.sh"]
 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD:-1234567890}
-# MYSQL_PASSWORD=${MYSQL_PASSWORD:-123456}
+MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD:-1234567890}
+MYSQL_PASSWORD=${MYSQL_PASSWORD:-123456}
 
-# PGSQL_ROOT_PASSWORD=${PGSQL_ROOT_PASSWORD:-1234567890}
-# PGSQL_PASSWORD=${PGSQL_PASSWORD:-123456}
+PGSQL_ROOT_PASSWORD=${PGSQL_ROOT_PASSWORD:-1234567890}
+PGSQL_PASSWORD=${PGSQL_PASSWORD:-123456}
 
 # init nginx
 if [ ! -d "/var/tmp/nginx/client_body" ]; then
@@ -13,63 +13,63 @@ if [ ! -d "/var/tmp/nginx/client_body" ]; then
 fi
 
 # init mysql
-# if [ ! -f "/run/mysqld/.init" ]; then
-#   [[ "$MYSQL_USER" = "root" ]] && echo "Please set MYSQL_USER other than root" && exit 1
+if [ ! -f "/run/mysqld/.init" ]; then
+  [[ "$MYSQL_USER" = "root" ]] && echo "Please set MYSQL_USER other than root" && exit 1
 
-#   SQL=$(mktemp)
+  SQL=$(mktemp)
 
-#   mkdir -p /run/mysqld /var/lib/mysql
-#   chown mysql:mysql -R /run/mysqld /var/lib/mysql
-#   sed -i -e 's/skip-networking/skip-networking=0/' /etc/my.cnf.d/mariadb-server.cnf
-#   mysql_install_db --user=mysql --datadir=/var/lib/mysql
+  mkdir -p /run/mysqld /var/lib/mysql
+  chown mysql:mysql -R /run/mysqld /var/lib/mysql
+  sed -i -e 's/skip-networking/skip-networking=0/' /etc/my.cnf.d/mariadb-server.cnf
+  mysql_install_db --user=mysql --datadir=/var/lib/mysql
 
-#   if [ -n "$MYSQL_DATABASE" ]; then
-#     echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $SQL
-#   fi
+  if [ -n "$MYSQL_DATABASE" ]; then
+    echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $SQL
+  fi
 
-#   MYSQL_DATABASE=${MYSQL_DATABASE:-*}
+  MYSQL_DATABASE=${MYSQL_DATABASE:-*}
 
-#   if [ -n "MYSQL_USER" ]; then
-#     echo "GRANT ALL ON $MYSQL_DATABASE.* to '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $SQL
-#     echo "GRANT ALL ON $MYSQL_DATABASE.* to '$MYSQL_USER'@'127.0.0.1' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $SQL
-#     echo "GRANT ALL ON $MYSQL_DATABASE.* to '$MYSQL_USER'@'::1' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $SQL
-#   fi
+  if [ -n "MYSQL_USER" ]; then
+    echo "GRANT ALL ON $MYSQL_DATABASE.* to '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $SQL
+    echo "GRANT ALL ON $MYSQL_DATABASE.* to '$MYSQL_USER'@'127.0.0.1' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $SQL
+    echo "GRANT ALL ON $MYSQL_DATABASE.* to '$MYSQL_USER'@'::1' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $SQL
+  fi
 
-#   echo "UPDATE mysql.user SET Password=PASSWORD('$MYSQL_ROOT_PASSWORD') WHERE User='root';" >> $SQL
-#   echo "DELETE FROM mysql.user WHERE User = '' OR Password = '';" >> $SQL
-#   echo "FLUSH PRIVILEGES;" >> $SQL
+  echo "UPDATE mysql.user SET Password=PASSWORD('$MYSQL_ROOT_PASSWORD') WHERE User='root';" >> $SQL
+  echo "DELETE FROM mysql.user WHERE User = '' OR Password = '';" >> $SQL
+  echo "FLUSH PRIVILEGES;" >> $SQL
 
-#   cat "$SQL" | mysqld --user=mysql --bootstrap --silent-startup --skip-grant-tables=FALSE
+  cat "$SQL" | mysqld --user=mysql --bootstrap --silent-startup --skip-grant-tables=FALSE
 
-#   rm -rf ~/.mysql_history ~/.ash_history $SQL
-#   touch /run/mysqld/.init
+  rm -rf ~/.mysql_history ~/.ash_history $SQL
+  touch /run/mysqld/.init
 # fi
 
 # init pgsql
-# if [ ! -f /run/postgresql/.init ]; then
-#   [[ "$PGSQL_USER" = "postgres" ]] && echo "Please set PGSQL_USER other than postgres" && exit 1
+if [ ! -f /run/postgresql/.init ]; then
+  [[ "$PGSQL_USER" = "postgres" ]] && echo "Please set PGSQL_USER other than postgres" && exit 1
 
-#   SQL=$(mktemp)
+  SQL=$(mktemp)
 
-#   mkdir -p /run/postgresql /usr/local/pgsql/data
-#   chown postgres:postgres -R /run/postgresql /usr/local/pgsql/data $SQL
-#   su postgres -c "initdb -D /usr/local/pgsql/data"
+  mkdir -p /run/postgresql /usr/local/pgsql/data
+  chown postgres:postgres -R /run/postgresql /usr/local/pgsql/data $SQL
+  su postgres -c "initdb -D /usr/local/pgsql/data"
 
-#   PGSQL_DATABASE=${PGSQL_DATABASE:-test}
-#   echo "CREATE DATABASE $PGSQL_DATABASE;" >> $SQL
-#   echo "ALTER USER postgres PASSWORD '$PGSQL_ROOT_PASSWORD';" >> $SQL
-#   if [ -n "$PGSQL_USER" ]; then
-#     echo "CREATE USER $PGSQL_USER WITH ENCRYPTED PASSWORD '$PGSQL_PASSWORD';" >> $SQL
-#     echo "GRANT ALL PRIVILEGES ON DATABASE $PGSQL_DATABASE TO $PGSQL_USER;" >> $SQL
-#   fi
-#   echo "GRANT ALL PRIVILEGES ON DATABASE $PGSQL_DATABASE TO postgres;" >> $SQL
+  PGSQL_DATABASE=${PGSQL_DATABASE:-test}
+  echo "CREATE DATABASE $PGSQL_DATABASE;" >> $SQL
+  echo "ALTER USER postgres PASSWORD '$PGSQL_ROOT_PASSWORD';" >> $SQL
+  if [ -n "$PGSQL_USER" ]; then
+    echo "CREATE USER $PGSQL_USER WITH ENCRYPTED PASSWORD '$PGSQL_PASSWORD';" >> $SQL
+    echo "GRANT ALL PRIVILEGES ON DATABASE $PGSQL_DATABASE TO $PGSQL_USER;" >> $SQL
+  fi
+  echo "GRANT ALL PRIVILEGES ON DATABASE $PGSQL_DATABASE TO postgres;" >> $SQL
 
-#   su postgres -c "pg_ctl -D '/usr/local/pgsql/data' -o '-c listen_addresses='' -p ${PGSQL_PORT:-5432}' -w start"
-#   su postgres -c "psql -f '$SQL'"
-#   rm -rf ~/.psql_history ~/.ash_history $SQL
-#   su postgres -c "pg_ctl -D '/usr/local/pgsql/data' -m fast -w stop"
-#   sed -i -E 's/host\s+all(.*)trust/host    all\1password/' /usr/local/pgsql/data/pg_hba.conf
-#   touch /run/postgresql/.init
-# fi
+  su postgres -c "pg_ctl -D '/usr/local/pgsql/data' -o '-c listen_addresses='' -p ${PGSQL_PORT:-5432}' -w start"
+  su postgres -c "psql -f '$SQL'"
+  rm -rf ~/.psql_history ~/.ash_history $SQL
+  su postgres -c "pg_ctl -D '/usr/local/pgsql/data' -m fast -w stop"
+  sed -i -E 's/host\s+all(.*)trust/host    all\1password/' /usr/local/pgsql/data/pg_hba.conf
+  touch /run/postgresql/.init
+fi
 
 exec "$@"
